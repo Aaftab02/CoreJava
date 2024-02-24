2c87b365eeec477141acea25dbbb433262ff065b
lenovo <lenovo@lenovo-PC-8>
2024-02-24 12:09:42 +0530

    Fifth Commit

---
+++ Assignment/src/com/edubridge/assignmentproblems/CountingVowelsAndConsonants.java
@@ -0,0 +1,33 @@
+package com.edubridge.assignmentproblems;
+import java.util.Scanner;
+//Check the 
+public class CountingVowelsAndConsonants {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the string: ");
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		input = input.toLowerCase();
+		
+		int vowelCount = 0;
+		int consonantCount = 0;
+		for(int i=0;i<input.length();i++)
+		{
+			char ch = input.charAt(i);
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+			{
+				vowelCount++;
+			}
+			else
+			{
+				consonantCount++;
+			}
+		}
+		System.out.println("Number of vowels present in the string: "+vowelCount);
+		System.out.println("Number of consonant present in the string: "+consonantCount);
+		sc.close();
+	}
+
+}
